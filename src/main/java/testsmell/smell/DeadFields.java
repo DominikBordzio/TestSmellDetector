ad94473afba065c91f42cbefff9a68f38e19c619
Dominik Bordzio <dbordzio@gmail.com>
2021-05-12 15:02:32 +0200

    Finish dead fields smell

--- src/main/java/testsmell/smell/DeadFields.java
+++ src/main/java/testsmell/smell/DeadFields.java
@@ -7,54 +7,55 @@ import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import testsmell.*;
+import testsmell.AbstractSmell;
+import testsmell.SmellyElement;
+import testsmell.TestMethod;
+import testsmell.Util;
 
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class DeadFields extends AbstractSmell{
 
     private List<MethodDeclaration> testMethods;
     private List<FieldDeclaration> testFields;
-    private List<String> strTestFields;
     private List<String> usedFields;
+    private boolean smelly = false;
 
     public DeadFields() {
         testMethods = new ArrayList<>();
         testFields = new ArrayList();
-        strTestFields = new ArrayList<>();
         usedFields = new ArrayList<>();
     }
 
     @Override
     public void runAnalysis(CompilationUnit testFileCompilationUnit, CompilationUnit productionFileCompilationUnit, String testFileName, String productionFileName) throws FileNotFoundException {
+        System.out.println("running");
         DeadFields.ClassVisitor classVisitor;
         classVisitor = new DeadFields.ClassVisitor();
         classVisitor.visit(testFileCompilationUnit, null);
-
-        strTestFields = strTestFields.stream().distinct().collect(Collectors.toList());
 
         for (MethodDeclaration method : testMethods) {
             classVisitor.visit(method, null);
         }
 
         usedFields = usedFields.stream().distinct().collect(Collectors.toList());
+        smelly = usedFields.size() < testFields.size();
+        System.out.println(smelly);
     }
 
     @Override
     public String getSmellName() {
-        return null;
+        return "Dead Fields";
     }
 
     @Override
     public boolean getHasSmell() {
-        return false;
+        return smelly;
     }
 
     @Override
@@ -81,6 +82,26 @@ public class DeadFields extends AbstractSmell{
                     testFields.add((FieldDeclaration) members.get(i));
                 }
             }
+        }
+
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            if (Util.isValidTestMethod(n)) {
+                super.visit(n, arg);
+                testMethod = new TestMethod(n.getNameAsString());
+                getAllChildNodes(n);
+            }
+        }
+
+        //Get all NameExpr inside of testMethod
+        public void getAllChildNodes(Node n){
+            List<Node> children = n.getChildNodes();
+            children.stream().forEach(x -> {
+                if(x instanceof NameExpr) {
+                    usedFields.add(((NameExpr) x).getNameAsString());
+                }
+                else getAllChildNodes(x);
+            });
         }
     }
 }
